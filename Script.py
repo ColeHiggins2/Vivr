7cd83ffac7f3b4d1d96b0353755d65a634dbae81
Colehiggins2 <colehiggins2@gmail.com>
2019-08-19 13:59:59 -0400

    cleaned up code

--- Script.py
+++ Script.py
@@ -1,32 +1,28 @@
-import copy
-import numpy as np
 import cv2
 import os
-from os.path import exists
-from PIL import Image, ImageTk, ImageFilter
 import Vivr as Vivr
-from PIL import Image
 
 
-img = cv2.imread('img-00.jpg')
-path = '/Users/coleh/Desktop/Vivr/temp'
-print(img)
-i = 0
+def Main():
+    img = cv2.imread('img-00.jpg')
+    path = '/Users/coleh/Desktop/Vivr/temp'
 
-while i <11:
-    flip = cv2.flip(img, 1)
-    cv2.imwrite(os.path.join(path, 'img.%5d.jpeg' % (i)), flip)
-    i += 1
-    flip = cv2.flip(flip, 1)
-    cv2.imwrite(os.path.join(path, 'img.%5d.jpeg' % (i)), flip)
-    i += 1
-    if i == 4:
-        for i in range(4, 8):
+    i = 0
+    while i < 11:
+        flip = cv2.flip(img, 1)
+        cv2.imwrite(os.path.join(path, 'img.%5d.jpeg' % (i)), flip)
+        i += 1
+        flip = cv2.flip(flip, 1)
+        cv2.imwrite(os.path.join(path, 'img.%5d.jpeg' % (i)), flip)
+        i += 1
+        if i == 4:
+            for i in range(4, 8):
                 cv2.imwrite(os.path.join(path, 'img.%5d.jpeg' % (i)), img)
                 i += 1
 
-Vivr.video(path, video_name = "MyVivr.mp4")
+    Vivr.Images.video(path, video_name="MyVivr.mp4")
+    Vivr.Images.delete_images(path)
 
-for file in os.scandir(path):
-    if file.name.endswith(".jpeg"):
-        os.unlink(file.path)
+
+if __name__ == "__main__":
+    Main()
--- Vivr.py
+++ Vivr.py
@@ -1,49 +1,45 @@
-#Look at life from the other side
-#import
-import copy
-import numpy as np
+# Look at life from the other side
 import cv2
 import os
-from os.path import exists
-from PIL import Image, ImageTk, ImageFilter
+from PIL import Image
 
 
-#read image in a flip flip image
-def flip_image(im):
-    org = Image.open(im).convert('RGB')
-    new=Image.new("RGB",org.size)
-    #print(type(org))
-    orgpixels, newpixels = org.load(), new.load()
-    for x in range(org.size[0]):
-        flipped_x = org.size[0] - x - 1
-        for y in range(org.size[1]):
-            pixel=orgpixels[x, y]
-            newpixels[flipped_x, y] = pixel
+# read image in and flips image
+class Images:
 
-#store images
+    def flip_image(im):
+        org = Image.open(im).convert('RGB')
+        new = Image.new("RGB", org.size)
+        orgpixels, newpixels = org.load(), new.load()
+        for x in range(org.size[0]):
+            flipped_x = org.size[0] - x - 1
+            for y in range(org.size[1]):
+                pixel = orgpixels[x, y]
+                newpixels[flipped_x, y] = pixel
+
+    # convert to video
+    def video(image_folder, video_name):
+        images = [img for img in os.listdir(image_folder) if img.endswith(".jpeg")]
+        print(images)
+        frame = cv2.imread(os.path.join(image_folder, images[0]))
+        height, width, layers = frame.shape
+
+        video = cv2.VideoWriter(video_name, 0, 2, (width, height))
+        for image in images:
+            video.write(cv2.imread(os.path.join(image_folder, image)))
+
+        cv2.destroyAllWindows()
+        video.release()
+
+    # delete instance images
+    def delete_images(path):
+        for file in os.scandir(path):
+            if file.name.endswith(".jpeg"):
+                os.unlink(file.path)
+
+
 """
-def store_image(im):
-    image_list = []
-    image = Image.open(im).convert('RGB')
-    print(type(image))
-    image_list.append(image)
-    print(image_list)
+if __name__ == '__main__':
+    image_folder =  '/Users/coleh/Desktop/Flikr/temp'
+    video(image_folder, video_name = "NewVivr.mp4")
 """
-#convert to video
-def video(image_folder, video_name):
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpeg")]
-    print(images)
-    frame = cv2.imread(os.path.join(image_folder, images[0]))
-    height, width, layers = frame.shape
-
-    video = cv2.VideoWriter(video_name, 0, 2, (width,height))
-
-    for image in images:
-        video.write(cv2.imread(os.path.join(image_folder, image)))
-
-    cv2.destroyAllWindows()
-    video.release()
-
-#if __name__ == '__main__':
-    #image_folder =  '/Users/coleh/Desktop/Flikr/temp'
-    #video(image_folder, video_name = "NewVivr.mp4")
